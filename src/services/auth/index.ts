e5a390e661afc3f692796ab0c6d8ee821f43e434
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-05-20 17:18:44 -0300

    feat: removed register from service

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,32 +1,10 @@
 import { isAxiosError } from "axios";
 import api from "../api";
-import { IAuth } from "./DTO";
+import { LoginDTO } from "./DTO";
 
-export const register = async (params: IAuth.IRegister.Params) => {
+export const LoginService = async (params: LoginDTO.Params) => {
   try {
-    const response = await api.post<IAuth.IRegister.Response>("/auth", params);
-    return {
-      success: true,
-      message: "Usuário cadastrado com sucesso",
-      data: response.data.data,
-    };
-  } catch (error) {
-    if (isAxiosError(error)) {
-      return {
-        success: false,
-        message: error.response?.data?.message || error.message,
-      };
-    }
-    return {
-      success: false,
-      message: "Erro desconhecido ao cadastrar usuário",
-    };
-  }
-};
-
-export const login = async (params: IAuth.ILogin.Params) => {
-  try {
-    const response = await api.post<IAuth.ILogin.Response>("/auth/sign_in", params);
+    const response = await api.post<LoginDTO.Response>("/auth/sign_in", params);
     return {
       success: true,
       message: "Login realizado com sucesso",
