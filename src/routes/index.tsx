3086bb3736827a2a736ebac22284ac258d55b188
marco-duart <marcoaurelio_db@hotmail.com>
2025-05-22 22:18:25 -0300

    feat: added new route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { PrivateRoute } from "../components/private-route";
 import { UserDTO } from "../services/user/DTO";
 import { useAuth } from "../contexts/auth-context";
+import { BaseLayout } from "../components/common/base-layout";
 import { LoginPage } from "../pages/login";
+import { HomePage } from "../pages/home";
 import { LoadingSpinner } from "../components/loading-spinner";
 
 export default function Router() {
-  const { isLoading, user } = useAuth();
+  const { isLoading } = useAuth();
 
   if (isLoading) {
     return <LoadingSpinner />;
@@ -16,6 +18,17 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route
+          element={
+            <PrivateRoute
+              requiredRole={[UserDTO.Role.EMPLOYEE, UserDTO.Role.MANAGER]}
+            >
+              <BaseLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route path="/home" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
