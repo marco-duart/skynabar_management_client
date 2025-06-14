c110b2a0a27efce24665971ae1a5b92efbdca1b2
marco-duart <marcoaurelio_db@hotmail.com>
2025-06-14 14:47:02 -0300

    refactor: changed dto name

--- src/helpers/build-products-filters.ts
+++ src/helpers/build-products-filters.ts
@@ -1,10 +1,10 @@
-import { IProduct } from "../services/products/DTO";
+import { ProductDTO } from "../services/products/DTO";
 
 export const buildProductFilters = (filters: {
   name?: string;
   sku?: string;
   productCategoryId?: number;
-}): IProduct.IGetProducts.Params => {
+}): ProductDTO.IGetProducts.Params => {
   const q: any = {};
 
   if (filters.name) q.name_cont = filters.name;
