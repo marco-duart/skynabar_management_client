469cd029e503dbd10b8eb91789da46f2a3477fd0
marco-duart <marcoaurelio_db@hotmail.com>
2025-06-15 17:14:20 -0300

    feat: updated button component

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -5,8 +5,8 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: S.ButtonVariant;
   isLoading?: boolean;
   icon?: ReactNode;
-  iconPosition?: 'left' | 'right';
-  size?: 'small' | 'medium' | 'large';
+  iconPosition?: "left" | "right";
+  size?: "small" | "medium" | "large";
 }
 
 export const Button = ({
@@ -14,31 +14,27 @@ export const Button = ({
   children,
   isLoading = false,
   icon,
-  iconPosition = 'left',
-  size = 'medium',
+  iconPosition = "left",
+  size = "medium",
   ...props
 }: Props) => {
   return (
-    <S.ButtonContainer 
-      variant={variant} 
-      isLoading={isLoading}
-      hasIcon={!!icon}
-      iconPosition={iconPosition}
-      size={size}
+    <S.ButtonContainer
+      $variant={variant}
+      $isLoading={isLoading}
+      $hasIcon={!!icon}
+      $iconPosition={iconPosition}
+      $size={size}
       {...props}
     >
-      {icon && iconPosition === 'left' && (
-        <S.IconWrapper position="left">
-          {icon}
-        </S.IconWrapper>
+      {icon && iconPosition === "left" && (
+        <S.IconWrapper position="left">{icon}</S.IconWrapper>
       )}
-      
+
       {children}
-      
-      {icon && iconPosition === 'right' && (
-        <S.IconWrapper position="right">
-          {icon}
-        </S.IconWrapper>
+
+      {icon && iconPosition === "right" && (
+        <S.IconWrapper position="right">{icon}</S.IconWrapper>
       )}
     </S.ButtonContainer>
   );
